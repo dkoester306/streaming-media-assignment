f6aeeb358b6d9ffe746d51df26c104ae8f4dc29c
Daniel Koester <dkoester306@gmail.com>
2020-02-04 13:04:13 -0500

    changed default port from 300 to 3000

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
 const mediaHandler = require('./mediaResponses.js');
 
-const port = process.env.PORT || process.env.NODE_PORT || 300;
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   console.log(request.url);
